71b46e8ead09c903d60f57e030243d0a41970f53
Thlamz <thlamenza@hotmail.com>
2020-05-21 18:20:15 -0300

    Evolução do CSS

--- Implementação/frontend/src/App.vue
+++ Implementação/frontend/src/App.vue
@@ -1,25 +1,45 @@
 <template>
   <div id="app">
-    Página principal
+    <navbar>RevisionHub</navbar>
+    <router-view id="router-view"></router-view>
   </div>
 </template>
 
 <script>
-
+import Navbar from './components/Navbar'
 export default {
   name: 'App',
   components: {
+    'navbar':Navbar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url(assets/css/main.css);
+  body {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: white;
+    margin: 0px;
+    background-color: var(--dark);
+    min-width: 500px;
+  }
+  #app
+  {
+    display:flex;
+    flex-direction: column;
+    height: 100vh;
+    width: 100vw;
+  }
+  navbar
+  {
+    flex: 0 0 auto;
+  }
+  #router-view
+  {
+    flex: 1 1 auto;
+  }
 </style>
--- Implementação/frontend/src/components/ChecklistChoice.vue
+++ Implementação/frontend/src/components/ChecklistChoice.vue
@@ -1,13 +1,80 @@
 <template>
-    <h1>Choice</h1>
+    <div class="main-container">
+        <div id="text-box">
+            <div id="main-text">Descubra Checklists</div>
+            <div id="secondary-text">
+                Uma aplicação web que te ajudará a achar a melhor lista de inspeção.
+            </div>
+        </div>
+        <div id="select-container">
+            <checklist-opcoes class="select-box"></checklist-opcoes>
+            <button>Go</button>
+        </div>
+
+    </div>
 </template>
 
 <script>
+    import CheckListOpcoes from './ChecklistOpcoes'
     export default {
-        name: "ChecklistChoice"
+        name: "ChecklistChoice",
+        components:
+        {
+            'checklist-opcoes': CheckListOpcoes
+        }
     }
 </script>
 
 <style scoped>
+    #text-box
+    {
+        color:var(--light);
+        text-align: justify;
+        width: min-content;
+        margin-bottom: 30px;
+    }
+    #main-text
+    {
+        font-size: 50px;
+        white-space: nowrap;
+        margin-bottom: 30px;
+    }
+    #secondary-text
+    {
+        font-size: 20px;
+    }
 
+    #select-container
+    {
+        width: 70%;
+        display:flex;
+        flex-direction: row;
+        justify-content: space-between;
+        min-width: 450px;
+        height: 50px;
+    }
+    .select-box
+    {
+        flex: 1 1 auto;
+        height:100%;
+    }
+    #select-container > button
+    {
+        flex: 0 1 auto;
+        padding: 12px 35px;
+        border-radius: 8px;
+        border: none;
+        color: var(--dark);
+        height:100%;
+    }
+
+    .main-container
+    {
+        text-align: left;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: baseline;
+        padding:20px;
+    }
 </style>
