812d4ee8423a4f65e596d69fc27d4f4702293c7d
Thlamz <thlamenza@hotmail.com>
2020-05-21 18:18:58 -0300

    Desenvolvendo navbar com icone placeholder

---
+++ Implementação/frontend/src/components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+    <nav>
+        <div class="navbar-title">
+            <img src="../assets/img/placeholder.png" width="90" height="100" alt="Icone">
+            <span class="title-text"><slot></slot></span>
+        </div>
+
+        <div class="button-group">
+            <button id="listButton">Minhas Listas</button>
+            <button id="logButton">Registrar</button>
+        </div>
+
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+    nav
+    {
+        display:flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+    }
+    .navbar-title
+    {
+        display: flex;
+        align-items: center;
+    }
+    .title-text
+    {
+        font-weight: 500;
+        font-size: 23px;
+    }
+    .button-group
+    {
+
+    }
+    .button-group > button
+    {
+        margin: 10px 25px;
+        padding: 12px 20px;
+        border-radius: 5px;
+        font-size: 17px;
+        color: white;
+    }
+    #listButton
+    {
+        background-color: var(--light);
+        border: none;
+    }
+    #logButton
+    {
+        background-color: var(--light-green);
+        border: none;
+    }
+</style>
