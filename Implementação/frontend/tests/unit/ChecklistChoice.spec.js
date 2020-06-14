01de837ebda6c3fec202074f98f76553d00801e3
Thlamz <thlamenza@hotmail.com>
2020-06-13 21:44:51 -0300

    Testes para ChecklistChoice e Mychecklists

--- Implementação/frontend/tests/unit/ChecklistChoice.spec.js
+++ Implementação/frontend/tests/unit/ChecklistChoice.spec.js
@@ -6,7 +6,7 @@ jest.mock("axios")
 
 describe('ChecklistChoice.vue', () => {
 
-    it('loads all checklists at mount', () => {
+    it('loads all checklists at mount', async () => {
         let mockData = [{
         title:'Mock',
         id: 1,
@@ -15,32 +15,24 @@ describe('ChecklistChoice.vue', () => {
         axios.create.mockImplementation((object) => axios)
         axios.get.mockImplementation((string) => Promise.resolve({data: mockData}))
         const wrapper = shallowMount(ChecklistChoice)
-        wrapper.vm.$nextTick(() => {
-            expect(axios.get).toHaveBeenCalledTimes(1)
-            expect(wrapper.vm.options).toStrictEqual(['Mock'])
-            expect(wrapper.vm.likes).toStrictEqual([10])
-            expect(wrapper.vm.values).toStrictEqual([1])
-        })
-
+        await wrapper.vm.$nextTick()
+        expect(axios.get).toHaveBeenCalledTimes(1)
+        expect(wrapper.vm.options).toStrictEqual(['Mock'])
+        expect(wrapper.vm.likes).toStrictEqual([10])
+        expect(wrapper.vm.values).toStrictEqual([1])
     })
-    it('alerts user of error in request and returns home', () => {
+
+    it('alerts user of error in request and returns home', async () => {
         axios.create.mockImplementation((object) => axios)
-        const mockString = "Mock"
-        axios.get.mockImplementation((string) => {throw Error(mockString)})
+        const error = new Error("Mock")
+        axios.get.mockImplementation((string) => {throw error})
         const $alert = jest.fn((string) => string)
-        const $router = {
-            push: jest.fn()
-        }
         const wrapper = shallowMount(ChecklistChoice, {
             mocks: {
-                $alert,
-                $router
+                $alert
             }
         })
-        wrapper.vm.$nextTick(() => {
-            expect($alert).toHaveReturnedWith(mockString)
-            expect($router.push).toHaveBeenCalled()
-        })
-
+        await wrapper.vm.$nextTick()
+        expect($alert).toHaveReturnedWith(error)
     })
 })
--- Implementação/frontend/tests/unit/ChecklistMyChecklists.spec.js
+++ Implementação/frontend/tests/unit/ChecklistMyChecklists.spec.js
@@ -2,18 +2,32 @@ import {createLocalVue, shallowMount} from '@vue/test-utils'
 import VueRouter from "vue-router";
 import axios from "axios"
 import ChecklistMyChecklists from "../../src/components/ChecklistMyChecklists";
+import ChecklistChoice from "../../src/components/ChecklistChoice";
 
 jest.mock("axios")
 
 describe('ChecklistChoice.vue', () => {
-    let mockData = [{
-        title:'Mock',
-        id: 1,
-        likes: 10
-    }]
-    it('redirects to home page if not authenticated', () => {
-        const $alert = jest.fn((string) => string)
+    it('shows the checklists returned on the request', async () => {
+        let mockData = [{
+            title:'Mock',
+            id: 1,
+            likes: 10
+        }]
+        axios.create.mockImplementation((object) => axios)
+        axios.get.mockImplementation((string) => Promise.resolve({data: mockData}))
+        const wrapper = shallowMount(ChecklistChoice)
+        await wrapper.vm.$nextTick()
+        expect(axios.get).toHaveBeenCalledTimes(1)
+        expect(wrapper.vm.options).toStrictEqual(['Mock'])
+        expect(wrapper.vm.likes).toStrictEqual([10])
+        expect(wrapper.vm.values).toStrictEqual([1])
+    })
 
+    it('redirects to home page and alerts if not authenticated', async () => {
+        const $alert = jest.fn((string) => string)
+        const $router = {
+            push: jest.fn()
+        }
         const wrapper = shallowMount(ChecklistMyChecklists, {
             computed:
             {
@@ -23,11 +37,43 @@ describe('ChecklistChoice.vue', () => {
             },
             mocks:
             {
-                $alert
+                $alert,
+                $router
+            }
+        })
+        await wrapper.vm.$nextTick()
+        expect(wrapper.vm.getAuthenticatedRoute).toThrow()
+        expect($alert).toHaveBeenCalled()
+        expect($router.push).toHaveBeenCalled()
+    })
+
+    it('alerts user of error in request and redirects home',async () => {
+        const error = new Error("Mock")
+        axios.get.mockImplementation((string) => {throw error})
+        const $alert = jest.fn((string) => string)
+        const wrapper = shallowMount(ChecklistMyChecklists, {
+            mocks: {
+                $alert,
+                $router: {
+                    push: jest.fn()
+                },
+                $store: {
+                    getters:
+                    {
+                        isConnected:true
+                    },
+                    state:
+                    {
+                        tokenStore:
+                        {
+                            token: '123'
+                        }
+                    }
+                }
             }
         })
 
-        expect(wrapper.vm.getAuthenticatedRoute).toThrow()
-        expect($alert).toHaveReturnedWith('Conexão expirada. Faça login novamente.')
+        await wrapper.vm.$nextTick()
+        expect($alert).toHaveReturnedWith(error)
     })
 })
